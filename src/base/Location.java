2ddbdd8affc9748e47089e3248ee304862b522a8
unknown <peeraphat4106@gmail.com>
2022-12-09 12:23:00 +0700

    try

---
+++ src/base/Location.java
@@ -0,0 +1,12 @@
+package base;
+
+import java.util.ArrayList;
+
+public class Location {
+	private ArrayList<Integer> fund;
+	private ArrayList<Integer> dictInLocation;
+	private int amountOfPlayer;
+
+	public Location(int amountOfPlayer) {
+	}
+}
