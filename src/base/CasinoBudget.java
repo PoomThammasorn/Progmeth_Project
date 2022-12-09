4226c310e86f3b391324ae0bb3c4640803e0aa61
unknown <peeraphat4106@gmail.com>
2022-12-09 16:45:53 +0700

    update location and Casino bugget

---
+++ src/base/CasinoBudget.java
@@ -0,0 +1,25 @@
+package base;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CasinoBudget {
+	private ArrayList<Banknote> banknoteTypeList;
+	public CasinoBudget() {
+		banknoteTypeList.add(new Banknote(10000));
+		banknoteTypeList.add(new Banknote(20000));
+		banknoteTypeList.add(new Banknote(30000));
+		banknoteTypeList.add(new Banknote(40000));
+		banknoteTypeList.add(new Banknote(50000));
+		banknoteTypeList.add(new Banknote(60000));
+		banknoteTypeList.add(new Banknote(70000));
+		banknoteTypeList.add(new Banknote(80000));
+		
+		
+	}
+	public ArrayList<Banknote> getBanknoteTypeList() {
+		Collections.shuffle(banknoteTypeList);
+		return banknoteTypeList;
+	}
+	
+}
--- src/player/DiceComparator.java
+++ src/player/DiceComparator.java
@@ -1,8 +1,12 @@
 package player;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
+import base.Banknote;
 import base.Dice;
+import base.SortByBanknoteValue;
 
 public class DiceComparator implements Comparator<Dice> {
 
@@ -11,5 +15,4 @@ public class DiceComparator implements Comparator<Dice> {
 		// TODO Auto-generated method stub
 		return d1.getPoint() - d2.getPoint();
 	}
-
 }
